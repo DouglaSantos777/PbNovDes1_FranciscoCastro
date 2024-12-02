68b8f29be04b068ba530eea85a66eed53cc97fbe
DouglaSantos777 <fdsantosdec@aluno.uespi.br>
2024-12-02 09:50:36 -0300

    fix: remove InvalidBoardingAttemptException class

--- src/main/java/com/challenge/scenario2/model/exceptions/InvalidBoardingAttemptException.java
+++
@@ -1,9 +0,0 @@
-package com.challenge.scenario2.model.exceptions;
-
-public class InvalidBoardingAttemptException extends Exception {
-    private static final long serialVersionUID = 1L;
-
-    public InvalidBoardingAttemptException(String msg) {
-        super(msg);
-    }
-}
