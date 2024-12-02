1523c43394a6044a6ccda8ee2ba2160a7368adb6
DouglaSantos777 <fdsantosdec@aluno.uespi.br>
2024-12-02 10:48:52 -0300

    feat: add .sql script for creating Person and Gondola tables

---
+++ src/main/java/com/challenge/scenario3/TablesPersonGondola.sql
@@ -0,0 +1,18 @@
+-- Como foi pedido SOMENTE as tabelas nao tem script para criar o banco
+
+CREATE TABLE Person (
+                        id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+                        name VARCHAR(100) NOT NULL,
+                        age INT NOT NULL,
+                        parent_id INT DEFAULT NULL,
+                        CONSTRAINT fk_parent FOREIGN KEY (parent_id) REFERENCES Person(id) ON DELETE SET NULL
+);
+
+CREATE TABLE Gondola (
+                         id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+                         gondola_number TINYINT UNSIGNED NOT NULL UNIQUE,
+                         seat1 INT DEFAULT NULL,
+                         seat2 INT DEFAULT NULL,
+                         CONSTRAINT fk_seat1 FOREIGN KEY (seat1) REFERENCES Person(id),
+                         CONSTRAINT fk_seat2 FOREIGN KEY (seat2) REFERENCES Person(id)
+);
