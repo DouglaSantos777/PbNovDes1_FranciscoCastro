1899a1fec91a27e2196bccd9dfa3304c9f9ac2b9
DouglaSantos777 <fdsantosdec@aluno.uespi.br>
2024-12-02 11:51:03 -0300

    feat: implement main method for emoji analysis

--- src/main/java/com/challenge/scenario4/Main.java
+++ src/main/java/com/challenge/scenario4/Main.java
@@ -4,6 +4,31 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("teste");
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Input: ");
+        String input = scan.nextLine();
+        String output = "";
+
+        int funnyEmojis = 0;
+        int upsetEmojis = 0;
+
+        for (int i = 0; i < input.length() - 2; i++) {
+            if (input.startsWith(":-)", i)) {
+                funnyEmojis++;
+            } else if (input.startsWith(":-(", i)) {
+                upsetEmojis++;
+            }
+        }
+
+        if (funnyEmojis > upsetEmojis) {
+            output = "funny";
+        } else if (upsetEmojis > funnyEmojis) {
+            output = "upset";
+        } else {
+            output = "neutral";
+        }
+
+        System.out.println("Output: " + output);
     }
 }
